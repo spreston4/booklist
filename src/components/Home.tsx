b7cb7e667c0d89a54faf5a65d95605185f2fdcd8
Sam Preston <sam.preston@headway.io>
2023-08-29 15:48:56 -0700

    registration form component posts new user to rails BE

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
+import Registration from "./auth/Registration";
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <Registration />
     </div>
   );
 };
---
+++ src/components/auth/Registration.tsx
@@ -0,0 +1,89 @@
+import * as React from "react";
+import axios from "axios";
+
+const Registration = () => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = React.useState("");
+  const [registrationErrors, setRegistrationErrors] = React.useState("");
+
+  const handleSubmit = (event: React.SyntheticEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (password !== passwordConfirmation) {
+      console.log("password error");
+      setRegistrationErrors("Passwords must match!");
+    } else {
+      axios
+        .post(
+          "http://localhost:3000/registrations",
+          {
+            user: {
+              email: email,
+              password: password,
+              password_confirmation: passwordConfirmation,
+            },
+          },
+          { withCredentials: true }
+        )
+        .then((response) => {
+          console.log("registration res: ", response);
+        })
+        .catch((error) => console.log("registration error", error));
+    }
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    switch (event.target.name) {
+      case "email": {
+        setEmail(event.target.value);
+        break;
+      }
+      case "password": {
+        setPassword(event.target.value);
+        break;
+      }
+      case "password_confirmation": {
+        setPasswordConfirmation(event.target.value);
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  };
+
+  return (
+    <div>
+      <h1>Resistration</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="password"
+          name="password_confirmation"
+          placeholder="Password Confirmation"
+          value={passwordConfirmation}
+          onChange={handleChange}
+          required
+        />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Registration;
