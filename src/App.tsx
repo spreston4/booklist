edc52ae6400f257335600b8af90b91a82c58cc0b
Sam Preston <sam.preston@headway.io>
2023-08-29 14:45:57 -0700

    Router set up in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <div>
-        <h1 className='text-red-700'>Woop</h1>
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
